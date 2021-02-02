6b99bb6e4c4bd89ed59f180a5f235d8f1925a8b7
Noah <noahnolte777@gmail.com>
2021-02-01 20:12:33 -0600

    Fixed some Thread killing problems

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ import threading
 print("\n----------------------------------\n")
 
 def time_keeper(timer_source):
+    global stop_timer
     print("bruh")
     obs_frontend_recording_start()
-    sleep(65)
+    for i in range(135):
+        if stop_timer:
+            return
+        else:
+            sleep(0.5)
     obs_frontend_recording_stop()
     obs_source_media_stop(timer_source)
     print("bruh moment 2.0")
     return
 
 def start_match(ignore1, ingore2):
-    global timer_source_name, time_thread
+    global timer_source_name, time_thread, stop_timer
+    stop_timer = False
     timer_source = obs_get_source_by_name(timer_source_name)
     time_thread = threading.Thread(target = time_keeper, args=(timer_source,))
     time_thread.start()
@@ -29,9 +35,9 @@ def start_match(ignore1, ingore2):
     timer_source = None
 
 def end_match(ignore1, ignore2):
-    global timer_source_name, time_thread
+    global timer_source_name, time_thread, stop_timer
     obs_frontend_recording_stop()
-    time_thread.stop()
+    stop_timer = True
     timer_source = obs_get_source_by_name(timer_source_name)
     obs_source_media_set_time(timer_source, 63000)
     obs_source_media_stop(timer_source)
